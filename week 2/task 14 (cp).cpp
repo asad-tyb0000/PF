c7a9d33c45a919ccebe472c6496cde8a23448c58
asad-tyb0000 <asad.tyb0000@gmail.com>
2023-09-15 09:34:18 -0700

    week 2

---
+++ week 2/task 14 (cp).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+main(){
+                              system("color 76");
+              cout << "                                                                             " << endl;
+              cout << "        '.--           ---,                                                          " << endl;
+              cout << "         ' .'!_...._/'._''                                                                   " << endl;
+              cout << "            !        /        ,                                                 " << endl;
+              cout << "            /()   () !      .' '-._                                                     " << endl;
+              cout << "           |)  .    ()!    /   _.'                                                    " << endl;
+              cout << "            |  _,_       ,;    <                                                    " << endl;
+              cout << "            ;.__       ,;|     |                                                   " << endl;
+              cout << "           / ,    / ,    |.-'.-'                                                 " << endl;
+              cout << "          (_/    (_/   ,;|.<'                                                              " << endl;
+              cout << "            !    ,       ;-'                                                      " << endl;
+              cout << "             >   !      /                                                       " << endl;
+              cout << "            (_,-''>  .'                                                                " << endl;
+              cout << "              (_,'                                                               " << endl;
+
+}
